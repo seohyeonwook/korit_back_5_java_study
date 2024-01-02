3ac8dd284a876d8b68359366abe30b4d29fad1ba
seohyeonwook <skut01541@gmail.com>
2024-01-02 16:15:38 +0900

    init: 수업자료최초커밋

---
+++ src/main/java/com/study/study/git.java
@@ -0,0 +1,39 @@
+package com.study.study;
+
+public class git {
+    // git - Local(WorkingDirectory, StagingArea, Repository)
+    // github - Remote(Repository)
+    /* git 쉽게 이해하려면 그냥 클라우드라고 생각해 
+       git(형상 관리) 사용 이유 -
+       1. 파일의 버전 관리
+       1-1) 버전이란 ?? ex) 1.3.15 버전일 경우 주.부.수 버전 순서대로
+            주 버전- 리뉴얼
+            부 버전- 추가적인 기능
+            수 버전- 버그/오류 수정 
+       git -
+       1) WorkingDirectory - 현 상태 -> 과거로 돌아갈 수 있음
+       2) StagingArea - WorkingDirectory 에서 원하는 파일을 선택해서 git add 해서 파일을 올리는 것
+       3) Repository - StagingArea 에서 git commit 해서 저장하는것
+       3-1) head - 내가 현재 작업중인 위치 / 커서라고 생각하자
+
+       2. 협업
+       .git 하나의 폴더만 있어야함
+       내가 살아가는 것을 복제를 해서 각각 살아가다가 나중에 합친다라는 개념
+       ex) 1번의 나는 일어나서 밥먹음 / 2번의 나는 일어나서 씻음 / 3번의 나는 일어나서 출근함
+           각자 나중에 합치면 일어나서(main) 합쳐서 밥먹고 씻고 출근하는게 됨
+       branch - 협업시 맡은 것을 작업
+       merge - 작업내용 합치기
+       branch 끼리 합치는걸 merge 한다고 한다
+       나뭇가지라고 생각하자 각자 뻗어나가다가 merge해서 mainbranch로 다시합침
+       이러한 행위들을 github - Remote(Repository) 라고 한다
+
+       clone - 깃허브 Repository에 있는 파일을 내 로컬 컴퓨터로 복사해오는 작업이다.
+       fetch - 원격 저장소에 있는 프로젝트를 로컬 저장소에 가져오는 것
+       폴더에서 우클릭 깃배스> 깃허프 페이지에서 깡통 만들기>
+        init > git status >add >commit> push
+
+
+    
+     */
+    
+}
